ff8437f45fae4b617e19379ee6d55dd5ff5ecb25
Ezra Oduor <ezrachurchill20@gmail.com>
2025-04-18 21:24:44 +0300

    added task table

---
+++ src/components/TaskTable.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+
+const TaskTable = ({ expenses, onDelete }) => {
+  return (
+    <div className="task-table">
+      <table>
+        <thead>
+          <tr>
+            <th>Task</th>
+            <th>Description</th>
+            <th>Category</th>
+            <th>when</th>
+            <th></th>
+          </tr>
+        </thead>
+        <tbody>
+          {expenses.map((expense, index) => (
+            <tr key={index}>
+              <td>{expense.task}</td>
+              <td>{expense.description}</td>
+              <td>{expense.category}</td>
+              <td>{expense.when}</td>
+              <td>
+                <button onClick={() => onDelete(index)}>Delete</button>
+               </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+  </div>
+  );
+};
+
+export default ExpenseTable;
