69bb0d212f31f2a17de69d79aa4694fe8d02442f
Ezra Oduor <ezrachurchill20@gmail.com>
2025-04-19 17:50:55 +0300

    added the styling

--- src/components/TaskForm.jsx
+++ src/components/TaskForm.jsx
@@ -6,7 +6,7 @@ const TaskForm = ({ form, onChange, onSubmit }) => {
       <input type="text" name="task" placeholder="Expense" value={form.expense} onChange={onChange} />
       <input type="text" name="description" placeholder="Description" value={form.description} onChange={onChange} />
       <input type="text" name="category" placeholder="Category" value={form.category} onChange={onChange} />
-      <input type="number" name="amount" placeholder="Amount" value={form.due} onChange={onChange} />
+      <input type="time" name="due" placeholder="Amount" value={form.due} onChange={onChange} />
       <button type="submit">Add Task</button>
     </form>
   );
